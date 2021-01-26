fdae216d1eb899cf91b41e8d8507703c2c0bc194
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-01-25 19:55:38 -0800

    removed comments

--- db.js
+++ db.js
@@ -1,22 +1,9 @@
-// The purpose of this module is to bring your Sequelize instance (`db`) together
-// with your models, for which you'll find some blank files in this directory:
-
 const Sequelize = require("sequelize");
 const pkg = require("./package.json");
 const chalk = require("chalk");
 
 const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}_test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
-
-// const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-//   logging: false,
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   ssl: true,
-//   dialectOptions: {
-//     ssl: true,
-//   },
-// });
 
 let db;
 
